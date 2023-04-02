39311c084da5d41bd959831f92226929adeab796
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-02 15:19:40 +0530

    made some changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,33 +13,36 @@ class _HomaPageState extends State<HomaPage> {
   SpeechToText speechToText = SpeechToText();
   var text = "hold the button and start speaking";
   var isListening = false;
+  var answer = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:  AvatarGlow(
+      floatingActionButton: AvatarGlow(
         endRadius: 60,
         animate: isListening,
         duration: const Duration(milliseconds: 2000),
         glowColor: Colors.pink,
         repeat: true, //
-        repeatPauseDuration: const Duration(milliseconds: 100),//
+        repeatPauseDuration: const Duration(milliseconds: 100), //
         showTwoGlows: true,
         child: GestureDetector(
           onTapDown: (details) async {
-            if(!isListening) {
+            if (!isListening) {
               var available = await speechToText.initialize();
-              if(available){
+              if (available) {
                 setState(() {
                   isListening = true;
                   speechToText.listen(
                     onResult: (result) {
                       setState(() {
-                        text = result.recognizedWords; 
+                        text = result.recognizedWords;
                       });
                     },
                   );
                 });
+                if (speechToText.isListening) {
+                }
               }
             }
           },
@@ -60,18 +63,20 @@ class _HomaPageState extends State<HomaPage> {
         centerTitle: true,
       ),
       body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(20),
-        margin: const EdgeInsets.only(bottom: 200),
-        child: Column(
-          children: [
-            Text(text, style: const TextStyle(fontSize: 22),),
-            const SizedBox(height: 170,),
-            const Text('Hold the mic and then say!')
-          ],
-        )
-        
-      ),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Text(
+                text,
+                style: const TextStyle(fontSize: 22),
+              ),
+              const SizedBox(
+                height: 170,
+              ),
+              const Text('Hold the mic and then say!'),
+            ],
+          )),
     );
   }
 }
